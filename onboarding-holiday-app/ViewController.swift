1520f600b290d605e251580e00f3333ced2cf4a5
Sherry1481 <shareyscorpi1@gmail.com>
2021-05-22 02:48:44 +0500

    player layer

--- onboarding-holiday-app/ViewController.swift
+++ onboarding-holiday-app/ViewController.swift
@@ -6,12 +6,15 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var darkView: UIView!
     @IBOutlet weak var GetStartedButton: UIButton!
     
+    private var player: AVPlayer?
+    private var playerLayer: AVPlayerLayer?
     
     
     override func viewDidLoad() {
@@ -25,6 +28,7 @@ class ViewController: UIViewController {
         super.viewWillAppear(animated)
         //for hiding nav bar
         navigationController?.setNavigationBarHidden(true, animated: animated)
+        setupPlayerIfNeeded()
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -40,7 +44,45 @@ class ViewController: UIViewController {
         GetStartedButton.layer.cornerRadius = 28
         GetStartedButton.layer.masksToBounds = true
     }
-
+    
+    private func buildPlayer() -> AVPlayer? {
+        guard let filepath = Bundle.main.path(forResource: "bg_video", ofType: "mp4") else {
+            return nil }
+        
+        let url = URL (fileURLWithPath: filepath)
+            let player = AVPlayer(url: url)
+        player.actionAtItemEnd = .none
+        player.isMuted = true
+        return player
+        }
+    
+    private func buildPlayerLayer() -> AVPlayerLayer? {
+        let layer = AVPlayerLayer(player: player)
+        layer.videoGravity = .resizeAspectFill
+        return layer
+    }
+    
+    private func playvideo() {
+        player?.play()
+    }
+    
+    private func restartVideo() {
+        player?.seek(to: .zero)
+    }
+    private func pauseVideo() {
+        player?.pause()
+    }
+    
+    func setupPlayerIfNeeded() {
+       player = buildPlayer()
+       playerLayer = buildPlayerLayer()
+        
+        if let layer = self.playerLayer,
+            view.layer.sublayers?.contains(layer) == false {
+            view.layer.insertSublayer(layer, at: 0)
+        }
+    }
+    
     @IBAction func getStartedButtonTapped(_ sender: Any) {
     }
     
